1897e5893ba87d56cb6e5e35d3c848dc3ed6e034
bloisejuli <jbloise@fi.uba.ar>
2023-08-22 11:49:54 -0300

    Mejora en la subida a base de datos

--- my_sql_connector.py
+++ my_sql_connector.py
@@ -1,32 +1,10 @@
-#from sqlalchemy import create_engine as create_engine_alchemy
+from sqlalchemy import create_engine
 
-#def create_engine():
-#    database_name = 'hardware_pricing'
-#    username = 'root'
-#    password = ''
-#    host = 'localhost'
-#    port = '3306'
-#    engine = create_engine_alchemy(f'mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database_name}')
-#    return engine
-
-import mysql.connector
-from sqlalchemy import create_engine as create_engine_alchemy
-
-def create_connection():
-    mydb = mysql.connector.connect(
-        host = 'localhost',
-        database_name = 'hardware_pricing',
-        user = 'root',
-        password = '',
-        port = '3306'
-    )
-    return mydb, mydb.cursor(buffered=True)
-
-def create_engine():
-    database_name = 'hardware_pricing',
-    username = 'root',
-    password = '',
-    host = 'localhost',
-    port = '3306',
-    engine = create_engine_alchemy(f'mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database_name}')
+def create_engine_mysql():
+    database_name = 'hardware_pricing'
+    username = 'root'
+    password = 'oycobe'
+    host = 'localhost'
+    port = 3306
+    engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database_name}')
     return engine
--- scraper_mexx.py
+++ scraper_mexx.py
@@ -1,8 +1,8 @@
 import requests
 import lxml.html.soupparser as sp
-import csv
 import pandas
-from my_sql_connector import create_engine
+from my_sql_connector import create_engine_mysql
+import datetime
 
 def get_page_from_url(url: str):
     session = requests.Session()
@@ -18,13 +18,11 @@ def get_page_from_url(url: str):
     data = response.content.decode('utf-8')
     return data
 
-
 def get_page_parsed(url: str):
     page = get_page_from_url(url)
     parsed_page = ''
     parsed_page = sp.fromstring(page)
     return parsed_page
-
 
 def get_text_or_not_found(elements):
     if elements:
@@ -33,51 +31,41 @@ def get_text_or_not_found(elements):
         return "Not found"
 
 def extract_data(item,category):
-    discount_applied = get_text_or_not_found(item.xpath("span"))
-    product = get_text_or_not_found(item.xpath(".//h4/a"))
+    title = get_text_or_not_found(item.xpath(".//h4/a"))
     link = item.xpath("div/div[contains(@class,'overlay')]/a/@href")[0]
     price_elements = item.xpath(".//div[contains(@class, 'price')]/h4/b")
-    discount_price = get_text_or_not_found(price_elements)
-    #price = 
-
+    cash_price = get_text_or_not_found(price_elements)
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     return {
-        "Category": category,
-        "Discount": discount_applied,
-        "Product": product,
-        "Discount_Price": discount_price,
-        "Link": link
+        "category": category,
+        "title": title,
+        "cash_price": cash_price,
+        "link": link,
+        "created_at": current_date
     }
 
-def print_data(data_list):
-    for item_data in data_list:
-        print("Discount:", item_data["Discount"])
-        print("Product:", item_data["Product"])
-        print("Price:", item_data["Discount_Price"])
-        print("Link:", item_data["Link"])
-        print("-" * 20)
 
-url_base = 'https://www.mexx.com.ar/productos-rubro/{}/?all=1'
-
-categories = ['notebooks', 'motherboards', 'memorias-ram', 'procesadores','almacenamiento','placas-de-video','fuentes-de-poder','gabinetes','refrigeracion-pc','combos-actualizacion-pc','teclados-mouses-y-pads','auriculares-y-microfonos-','camaras-web-e-ip','monitores','conectividad-y-redes','volantes-y-gamepads','outlet']
+if __name__ == '__main__':
+    categories = ['notebooks', 'motherboards', 'memorias-ram', 'procesadores','almacenamiento','placas-de-video','fuentes-de-poder','gabinetes','refrigeracion-pc','combos-actualizacion-pc','teclados-mouses-y-pads','auriculares-y-microfonos-','camaras-web-e-ip','monitores','conectividad-y-redes','volantes-y-gamepads','outlet']
+    data_list = []
 
 
-data_list = []
+    for category in categories:
+        page = get_page_parsed(f'https://www.mexx.com.ar/productos-rubro/{category}/?all=1')
+        items = page.xpath("//div[contains(@class,'productos')]")
+        for item in items:
+            data = extract_data(item, category)
+            data_list.append(data)
+            
 
+    myvar = pandas.DataFrame.from_records(data_list)
+    print(myvar)
 
-for category in categories:
-    page = get_page_parsed(f'https://www.mexx.com.ar/productos-rubro/{category}/?all=1')
-    items = page.xpath("//div[contains(@class,'productos')]")
-    #print("AAAAAAAAAAAAAAAAAAAAAAA")
-    #print(f'https://www.mexx.com.ar/productos-rubro/{category}/?all=1')
-    for item in items:
-        data = extract_data(item, category)
-        data_list.append(data)
-        
-#print_data(data_list)
+    engine = create_engine_mysql()
 
-
-myvar = pandas.DataFrame.from_records(data_list)
-connection = create_engine()
-myvar.to_sql("products", connection, if_exists = 'append', index = False)
-print(myvar)
+    try:
+        myvar.to_sql("products", con=engine, if_exists='append', index=False)
+        print("Datos insertados en la base de datos.")
+    except Exception as e:
+        print("Error:", e)
